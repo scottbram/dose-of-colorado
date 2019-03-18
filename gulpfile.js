d396ac31e036e97ce261d6bf6bb0f9a3dee3ae4e
Scott Bram <thatbram@gmail.com>
2019-03-18 16:38:33 -0600

    Debugging faulty Netlify build
    Revisions to gulpfile.js & package.json

--- gulpfile.js
+++ gulpfile.js
@@ -15,10 +15,10 @@ var gulp = require('gulp'),
 	  'js': 'dist/js'
 	};
 
-// Process Sass files
-var sass_files_src = ['src/styles/*.scss', 'src/styles/**/*.scss'],
-    sass_files_dest = 'dist/styles';
+// Default task
+gulp.task('build', ['build-styles', 'build-js']);
 
+// Process Sass files
 gulp.task('build-styles', function () {
     return gulp.src(input.styles)
     	.pipe(sourcemaps.init())
@@ -38,15 +38,4 @@ gulp.task('build-js', function() {
         .pipe(uglify())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(output.js));
-});
-
-// Default task
-/*gulp.task('default', function () {
-    gulp.start(['sass', 'js']);
-});*/
-
-// gulp.task('build', ['build-styles', 'build-js']);
-
-gulp.task('build', function () {
-    gulp.start(['build-styles', 'build-js']);
 });
