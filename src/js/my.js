7642c342a97a20bbb86c411b0148ce3ec44de6dc
Scott Bram <thatbram@gmail.com>
2019-03-20 22:01:09 -0600

    Mored My

--- src/js/my.js
+++ src/js/my.js
@@ -1,5 +1,15 @@
 /** my.js */
 
-console.log('my.js');
+// console.log('my.js');
 
 let myid = 0;
+
+$('#mydoc_id_search_field').on('input', function () {
+	myid = $(this).val();
+
+	if (myid.length > 4) {
+		$('#mydoc_id_search_go').attr('disabled', 'false');
+	} else {
+		$('#mydoc_id_search_go').attr('disabled', 'true');
+	}
+});
