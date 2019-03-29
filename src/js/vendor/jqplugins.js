dace21cfa9d1065253eeb98fd9133dde0aeda1a0
Scott Bram <thatbram@gmail.com>
2019-03-28 19:57:39 -0600

    Fixed the whole myDoC id query string to search field mess
    Forgot that the query string should function as a search (or "search", currently) against stored data rather than behave as an actual known valid value

--- src/js/vendor/jqplugins.js
+++ src/js/vendor/jqplugins.js
@@ -3,7 +3,5 @@ $.urlParam = function (name) {
     var results = new RegExp('[\?&]' + name + '=([^&#]*)')
                       .exec(window.location.search);
 
-    var valueBeginsIdx = results[1].indexOf('=');
-
-    return (results !== null) ? results[1].substr(valueBeginsIdx, results[1].length) || 0 : false;
+    return (results !== null) ? results[1] || 0 : false;
 }
