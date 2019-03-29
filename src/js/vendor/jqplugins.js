2ed2114d18d8b71a164d7dfcec86f3e61823d945
Scott Bram <thatbram@gmail.com>
2019-03-28 19:40:36 -0600

    Change what jQ plugin urlParam returns

--- src/js/vendor/jqplugins.js
+++ src/js/vendor/jqplugins.js
@@ -3,5 +3,7 @@ $.urlParam = function (name) {
     var results = new RegExp('[\?&]' + name + '=([^&#]*)')
                       .exec(window.location.search);
 
-    return (results !== null) ? results[1] || 0 : false;
+    var valueBeginsIdx = results[1].indexOf('=');
+
+    return (results !== null) ? results[1].substr(valueBeginsIdx, results[1].length) || 0 : false;
 }
