8f9c8057fc2963d7676a0428b41e37307703737a
Scott Bram <thatbram@gmail.com>
2019-04-08 23:45:26 -0600

    Update mydoc_at.js
    And more

--- functions/mydoc_at.js
+++ functions/mydoc_at.js
@@ -2,10 +2,12 @@ exports.handler = async (event, context) => {
 	const mydocid = event.queryStringParameters.mydocid;
 
 	console.log(mydocid);
-	
-	/*const { AIRTABLE_API_KEY } = process.env;
 
-	var Airtable = require('airtable');
+	const { AIRTABLE_API_KEY } = process.env;
+
+	console.log(AIRTABLE_API_KEY);
+
+	/*var Airtable = require('airtable');
 	var base = new Airtable({apiKey: AIRTABLE_API_KEY}).base('appyIApZ1WBML8Rmo');
 
 	base('mydoc_locations').find(mydocid, function(err, record) {
