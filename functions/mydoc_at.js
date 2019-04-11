5e2f49099c90b27e9b2efc1835756f80820149b3
Scott Bram <thatbram@gmail.com>
2019-04-11 07:32:00 -0600

    Eureka! *throws papers over balcony*

--- functions/mydoc_at.js
+++ functions/mydoc_at.js
@@ -1,34 +1,56 @@
-exports.handler = async (event, context) => {
-
-	console.log('event: ');
-	console.log(event);
-
+exports.handler = function (event, context, callback) {
 	const mydocid = event.queryStringParameters.mydocid
 
 	console.log('mydocid: ' + mydocid);
 
 	const { AIRTABLE_API_KEY } = process.env
+	
 	const Airtable = require('airtable')
 
 	console.log('Airtable: ');
-	console.log(Airtable);
-	
+
 	const base = new Airtable({
 			apiKey: AIRTABLE_API_KEY
 		})
 		.base('appyIApZ1WBML8Rmo');
 
-	base('mydoc_locations').find(mydocid, function(err, record) {
-	    if (err) { console.error(err); return; }
-	    
-	    console.log(record);
+	var builtBody;
 
+	base('mydoc_locations').select({
+	    filterByFormula: "NOT({mydocid} = '')",
+	    maxRecords: 10,
+	    view: "Grid view"
+	}).eachPage(function page(records, fetchNextPage) {
+	    // This function (`page`) will get called for each page of records.
+
+	    builtBody = JSON.stringify(records);
+
+	    /*records.forEach(function(record) {
+	    	let strRet = 'Retrieved:' + record.get('mydocid')
+	        
+	        console.log(strRet);
+
+	    });*/
+
+	    // To fetch the next page of records, call `fetchNextPage`.
+	    // If there are more records, `page` will get called again.
+	    // If there are no more records, `done` will get called.
+	    fetchNextPage();
+
+	}, function done(err) {
+	    if (err) { console.error(err); return; }
+
+	    callback(null, {
+			statusCode: 200,
+			body: builtBody
+		});
 	});
 }
 
-/*exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Hello, World"
-  });
-};*/
+/*exports.handler = function (event, context, callback) {
+// exports.handler = async (event, context) => {
+
+	console.log('context: ');
+	console.log(context);
+
+}*/
